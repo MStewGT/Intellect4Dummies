86796fd2fd582b09af22ba3c645f82f26f9cfc27
Matt S <mstewgt@gmail.com>
2018-11-11 23:42:41 -0600

    Initial upload

---
+++ main.lua
@@ -0,0 +1,54 @@
+-- Main Lua code for Intellect4Dummies
+-- Author: MStewGT
+
+-- Returns a list of members in the player's party or raid and assigns to a variable
+raidMembers = {};
+partyMembers = {};
+do
+  for i=1,40 do
+    raidMembers[i] = "raid"..i
+  end
+  for i=1,4 do
+    partyMembers[i] = "party"..i
+  end
+end
+
+
+-- Loop through raidMembers and check Spell Power aura for Arcane Intellect
+function CheckBuffsGroup()
+  local needBuff = false
+  local aura = false
+  
+  if IsInRaid() then
+    for i=1, GetNumGroupMembers() do
+      aura = AuraUtil.FindAuraByName("Arcane Intellect", raidMembers[i])
+      if not aura then
+        needBuff = true
+        message("Someone is Dumb!!")
+        return needBuff
+      end
+    end
+    
+    elseif IsInGroup() then
+      for i=1, GetNumSubgroupMembers() do
+        aura = AuraUtil.FindAuraByName("Arcane Intellect", partyMembers[i])
+        if not aura then
+          needBuff = true
+          message("Someone is Dumb!!")
+          return needBuff
+        end
+      end        
+        
+    else
+      aura = AuraUtil.FindAuraByName("Arcane Intellect", "player")
+      if not aura then
+        needBuff = true
+        message("Someone is Dumb!!")
+        return needBuff
+      end
+        
+  end
+    
+end
+
+-- Event handler
